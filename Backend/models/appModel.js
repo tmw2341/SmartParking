f23793fe959bc8c5900f4797903947313e294b06
Tyler Witte <tylerwitte44@msn.com>
2018-03-25 11:32:46 -0500

    Made the app model.

---
+++ Backend/models/appModel.js
@@ -0,0 +1,34 @@
+class parkingSpot {
+    constructor(sensorID) {
+        this.sensorID = sensorID;
+        this.occupied = false;
+
+    };
+
+    parkCar() {
+        this.occupied = true;
+    }
+
+    unparkCar() {
+        this.occupied = false;
+    }
+
+    getID() {
+        return this.sensorID;
+    }
+}
+
+class parkingLot {
+    constructor(lotID) {
+        this.lotID = lotID;
+        this.sensors = [];
+    };
+
+    getID() {
+        return this.lotID;
+    }
+
+    addSpot(spot) {
+        this.sensors[this.sensors.length] = spot;
+    }
+}
