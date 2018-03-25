339e3b5447b831fac26ceb3c02e79a4f1474f259
Sammy Sherman <sfinasts@iastate.edu>
2018-03-25 17:38:34 -0500

    [Images] Added a temporary parking lot image. Also made images directory accessible

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/scripts", express.static( path.join(__dirname, "../Frontend/scripts")));
 app.use("/styles", express.static( path.join(__dirname, "../Frontend/styles")));
+app.use("/images", express.static( path.join(__dirname, "../Frontend/images")));
 
 /**
  |=========================
