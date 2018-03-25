f279f8b53fbfc7f3850226e87571e8d095531cbb
Sammy Sherman <sfinasts@iastate.edu>
2018-03-25 14:26:36 -0500

    [Routes] Made styles and scripts folders publicly accessible. Cleaned up routes.

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,17 @@ var app = express();
 var port = process.env.PORT || 3000;
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var path = require('path');
 var bodyParser = require('body-parser');
 
 //Use Jade templating.
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '../Frontend/views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use("/scripts", express.static( path.join(__dirname, "../Frontend/scripts")));
+app.use("/styles", express.static( path.join(__dirname, "../Frontend/styles")));
 
 /**
  |=========================
@@ -19,11 +22,8 @@ app.use(bodyParser.json());
  */
 //Landing page
 app.get('/', function(req, res){
-  res.render('index');
-});
-
-app.get('/test', function(req, res){
-  res.render('test');
+  console.log(__dirname);
+  res.render('home');
 });
 
 app.put('/sensor', function(req, res){
@@ -45,10 +45,6 @@ io.on('connection', function (socket) {
 app.get('/map', function (req, res) {
     res.render('map');
 });
-
-
-
-
 
 //Serve and listen on the set port.
 server.listen(port);
