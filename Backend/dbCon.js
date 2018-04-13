c906f13035659eb129649b0a87594b5509d1ec26
Tyler Witte <tylerwitte44@msn.com>
2018-04-13 17:53:31 -0500

    Added databases interaction class

--- Backend/dbCon.js
+++ Backend/dbCon.js
@@ -1,17 +1,87 @@
 const sqlite3 = require('sqlite3').verbose();
+let db = openDBCon();
+getAllSensorID(2);
+getSensorStatus(1, 1);
+addSensor(3);
+updateSensor(2, 3, 'F');
+closeDBCon(db);
 
-// open database in memory
-let db = new sqlite3.Database('db/testServer.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-});
+function addSensor(lotID) {
 
-// close the database connection
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
+    db.run(`INSERT INTO Sensors(lotID) VALUES(?)`, [lotID], function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Sensor inserted. ID: ${this.lastID}`);
+    });
+    return this.lastID;
+}
+
+function updateSensor(lotID, sensorID, signal) {
+    let data = [signal, sensorID, lotID];
+    let sql = `UPDATE Sensors
+            SET Status = ?
+            WHERE SensorID = ?
+            AND LotID = ?`;
+
+    db.run(sql, data, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Row(s) updated: ${this.changes}`);
+
+    });
+}
+
+function getAllSensorID(lotID) {
+    let sql = `SELECT DISTINCT SensorID FROM Sensors
+            WHERE LotID = `+ lotID + `
+           ORDER BY SensorID`;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            console.log(row.SensorID);
+        });
+    });
+}
+
+function getSensorStatus(lotID, sensorID) { // returns 
+    let sql = `SELECT DISTINCT Status FROM Sensors
+            WHERE LotID = `+ lotID + `
+            AND SensorID = ` + sensorID + `
+           ORDER BY SensorID`;
+    let response;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            response = row.Status;
+        });
+    });
+    return response;
+}
+
+function openDBCon() { // open database in memory
+    let db = new sqlite3.Database('db/testServer.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
+    return db;
+}
+
+function closeDBCon(db) { // close the database connection
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Database connection closed.');
+    });
+}
+
